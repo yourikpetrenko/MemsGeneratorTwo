3916792fe6eb1517c83d025a930f9eaae861eae5
yourikpetrenko <yourikpetrenko@gmail.com>
2020-08-12 10:53:47 +0300

    fix

---
+++ MemsGeneratorTwo/Presenter/MainViewPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewPresenter.swift
+//  MemsGeneratorTwo
+//
+//  Created by Jura on 12.08.2020.
+//  Copyright © 2020 Jura. All rights reserved.
+//
+
+import Foundation
+
+class MainViewPresenter {
+    
+    init() {
+        
+    }
+}
